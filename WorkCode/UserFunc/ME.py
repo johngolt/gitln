43d57e9d761f3639f89d38c1e651a6f454c3ee9f
golt <lyq212760@163.com>
2020-01-06 20:32:44 +0800

    modified files

--- WorkCode/UserFunc/ME.py
+++ WorkCode/UserFunc/ME.py
@@ -1,4 +1,4 @@
-import numpy as np 
+import numpy as np
 import pandas as pd
 from abc import abstractmethod
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder
@@ -7,7 +7,8 @@ from sklearn import metrics
 from pyecharts.components import Table
 from pyecharts.options import ComponentTitleOpts
 import xgboost as xgb
-import lightgbm as lgb 
+import lightgbm as lgb
+
 
 class ResultShow:
 
@@ -28,16 +29,16 @@ class ResultShow:
         x = list(range(len(train)))
         c = (Line(init_opts=opts.InitOpts(theme=ThemeType.LIGHT))
             .add_xaxis(x).add_yaxis("Train",ldic['train']['auc'],symbol='none',)
-            .add_yaxis("Valid",ldic['valid']['auc'],symbol='none',
+            .add_yaxis("Valid", ldic['valid']['auc'], symbol='none',
                     markline_opts=opts.MarkLineOpts(
-                    data=[opts.MarkLineItem(name='auc',x=index)],
+                    data=[opts.MarkLineItem(name='auc', x=index)],
                     label_opts=opts.LabelOpts(is_show=False)))
             .set_global_opts(title_opts=opts.TitleOpts(title=title),
                             xaxis_opts=opts.AxisOpts(type_="value")))
         return c
 
     def tabshow(self, res):
-        trains, valids=res['train'], res['valid']
+        trains, valids = res['train'], res['valid']
         tab = Tab(page_title='模型选择')
         for key in trains.keys():
             tab.add(self.line(trains[key], valids[key], key), key)
@@ -49,9 +50,9 @@ class ResultShow:
         return cate, bins[:-1][::-1]
 
     def bin_info(self, y, ypred):
-        columns=['箱','分数','样本数量','关注样本数','区间关注类别比例',
-        '累计关注类别比例','区间准确率','累计准确率','尾部准确率',
-        '提升','累计提升']
+        columns = ['箱', '分数', '样本数量', '关注样本数', '区间关注类别比例',
+        '累计关注类别比例', '区间准确率', '累计准确率', '尾部准确率',
+        '提升', '累计提升']
         cate, bins = self.get_group(y, ypred)
         df = pd.DataFrame(columns=columns, index=bins)
         group = y.groupby(cate)
@@ -66,8 +67,8 @@ class ResultShow:
         df['累计提升'] = 
 
     def confusin(self, y, ypred, bin_num=1):
-        columns=['实际类别','类别总数','正确百分比','预测类别 非关注','预测类别 关注']
-        indexs=['非关注','关注','合计']
+        columns = ['实际类别', '类别总数', '正确百分比', '预测类别 非关注', '预测类别 关注']
+        indexs = ['非关注', '关注', '合计']
         df = pd.DataFrame(columns=columns)
         df['实际类别'] = indexs
         df = df.set_index('实际类别')
@@ -77,27 +78,27 @@ class ResultShow:
         y1 = (ypred > bins[bin_num-1]).astype(int)
 
         df.iloc[:2, -2:] = metrics.confusion_matrix(y, y1)
-        df.loc['关注','正确百分比'] = df.at['关注','预测类别 关注']/df.at['关注','类别总数']
-        df.loc['非关注','正确百分比'] = df.at['非关注','预测类别 非关注']/df.at['非关注','类别总数']
-        df.loc['合计','预测类别 关注'] = df.at['关注','预测类别 关注']/df.loc[:,'预测类别 关注'].sum()
-        df.loc['合计','预测类别 非关注'] = df.at['非关注','预测类别 非关注']/df.loc[:,'预测类别 非关注'].sum()
+        df.loc['关注', '正确百分比'] = df.at['关注', '预测类别 关注']/df.at['关注', '类别总数']
+        df.loc['非关注', '正确百分比'] = df.at['非关注', '预测类别 非关注']/df.at['非关注', '类别总数']
+        df.loc['合计', '预测类别 关注'] = df.at['关注', '预测类别 关注']/df.loc[:, '预测类别 关注'].sum()
+        df.loc['合计', '预测类别 非关注'] = df.at['非关注', '预测类别 非关注']/df.loc[:, '预测类别 非关注'].sum()
         return df
 
     def indicate(self, y, ypred, bin_num=1):
         _, bins = self.get_group(y, ypred)
         y1 = (ypred > bins[bin_num-1]).astype(int)
-        columns=['名称','正确百分比']
-        indexs = ['准确率','精确率','召回率']
-        indexs2 = ['灵敏度','特异度','F1统计']
+        columns = ['名称', '正确百分比']
+        indexs = ['准确率', '精确率', '召回率']
+        indexs2 = ['灵敏度', '特异度', 'F1统计']
         df1 = pd.DataFrame(columns=columns)
         df1['名称'] = indexs
         df2 = pd.DataFrame(columns=columns)
         df2['名称'] = indexs2
         value = metrics.accuracy_score(y, y1), metrics.precision_score(y, y1), metrics.recall_score(y, y1)
-        df1.loc[:,'正确百分比'] = value
+        df1.loc[:, '正确百分比'] = value
         conf = metrics.confusion_matrix(y, y1)
-        value = conf[1,1]/conf.sum(axis=1)[1], conf[0,0]/conf.sum(axis=1)[0], metrics.f1_score(y, y1)
-        df2.loc[:,'正确百分比'] = value
+        value = conf[1, 1]/conf.sum(axis=1)[1], conf[0, 0]/conf.sum(axis=1)[0], metrics.f1_score(y, y1)
+        df2.loc[:, '正确百分比'] = value
         return df1, df
 
 
@@ -111,17 +112,13 @@ class PreprocessME:
     def testpreprocess(self, df_cat, df_num, **kwargs):
         pass
 
-    def set_params(self, **kwargs):
-        '''设定模型的参数，用于用户自定义模型参数。'''
-        self.est.set_params(**kwargs)
-
     def select_cat(self, X, columns=None):
         '''选取类别特征，默认所有的object类型的特征为类别，也可以自定义一些特征
         作为类别特征。'''
         result = X.select_dtypes(include='object')
         cat = result.columns
         if columns is not None:
-            cat = cat|set(columns)
+            cat = cat | set(columns)
         return cat
 
     def delete_columns(self, X, columns=None):
@@ -144,7 +141,7 @@ class PreprocessME:
         '''得到最终用于建模的数据。'''
         if y is None:
             self.num_col, self.cat_col = self.get_cat_num(X, drop_col, cat_col)
-            df_num, df_cat = X.loc[:, self.num_col], X.loc[:,self.cat_col]
+            df_num, df_cat = X.loc[:, self.num_col], X.loc[:, self.cat_col]
             Xtrain = self.trainpreprocess(df_cat, df_num)
         else:
             if hasattr(self, 'num_col') and hasattr(self, 'cat_col'):
@@ -154,6 +151,7 @@ class PreprocessME:
             else:
                 raise ValueError('模型没有训练，不能用于测试！')
         return Xtrain
+
 
 class LGBPreprocess(PreprocessME):
     def fillcat(self, X):
@@ -188,7 +186,6 @@ class LGBPreprocess(PreprocessME):
         return result
 
 
-
 class ModelME:
     def __init__(self):
         self.est = self.init_est()
@@ -199,6 +196,7 @@ class ModelME:
         pass
 
     def set_params(self, **kwargs):
+        '''设置模型的参数。'''
         self.est.set_params(**kwargs)
     
     def get_label(self, data):
@@ -212,7 +210,7 @@ class ModelME:
     def _lift(self, y, preds):
         groupor = pd.qcut(preds, q=10, duplicates='drop')
         result = y.groupby(groupor).mean()/label.mean()
-        return 'lift',result.max()
+        return result.max()
 
     @abstractmethod
     def lift(self, data, preds):
@@ -226,7 +224,7 @@ class ModelME:
         if self.name == 'XGBClassifier':
             params = self.est.get_xgb_params()
             params.pop('n_estimators')
-            params['eval_metric']='auc'
+            params['eval_metric'] = 'auc'
         elif self.name == 'LGBMClassifier':
             params = self.est.get_params()
             params.pop('n_estimators')
@@ -268,29 +266,30 @@ class ModelME:
     
     def train(self, X, y, imbalance=False):
         tdata, vdata = self.get_train_valid(X, y, imbalance)
-        params = self.get_params( )
-        res, res1 = { }, { }
+        params = self.get_params()
+        res, res1 = {}, {}
         if self.name == 'XGBClassifier':
             result = xgb.train(params, tdata, evals=[(tdata, 'train'), (vdata, 'valid')],
-                          num_boost_round=200, feval=self.ks, evals_result=res, 
-                   verbose_eval=False)
+                                num_boost_round=200, feval=self.ks, evals_result=res, 
+                                verbose_eval=False)
             result = xgb.train(params, tdata, evals=[(tdata, 'train'), (vdata, 'valid')],
-                    num_boost_round=200, feval=self.lift, evals_result=res1, 
-                            verbose_eval=False)
+                               num_boost_round=200, feval=self.lift, evals_result=res1, 
+                               verbose_eval=False)
         elif self.name == 'LGBMClassifier':
-            result = lgb.train(params,ltdata,num_boost_round=200,
-                    valid_sets=[ltdata,lvdata],valid_names=['train','valid'],
-                            feval=self.ks, verbose_eval=False,evals_result=res)
-            result = lgb.train(params,ltdata,num_boost_round=200,
-                    valid_sets=[ltdata,lvdata],valid_names=['train','valid'],
-                            feval=self.lift, verbose_eval=False,evals_result=res1)
+            result = lgb.train(params, ltdata, num_boost_round=200,
+                               valid_sets=[ltdata, lvdata], valid_names=['train', 'valid'],
+                               feval=self.ks, verbose_eval=False, evals_result=res)
+            result = lgb.train(params, ltdata, num_boost_round=200,
+                               valid_sets=[ltdata, lvdata], valid_names=['train', 'valid'],
+                               feval=self.lift, verbose_eval=False, evals_result=res1)
         res['train'].update(res1['train'])
         res['valid'].update(res1['valid'])
         return res, result  
 
+
 class XGBME(ModelME):
     def init_est(self):
-        params = { 'base_score': 0.5, 'booster': 'gbtree', 'colsample_bylevel': 1,
+        params = {'base_score': 0.5, 'booster': 'gbtree', 'colsample_bylevel':1,
                'colsample_bynode': 1, 'colsample_bytree': 0.5, 'gamma': 0,
                'learning_rate': 0.1, 'max_delta_step': 0, 'max_depth': 5,
                 'min_child_weight': 1, 'n_estimators': 200, 'reg_alpha': 0,
@@ -308,9 +307,10 @@ class XGBME(ModelME):
         res = self._lift(y, preds)
         return 'lift', res
 
+
 class LGBME(ModelME):
     def init_est(self):
-        params = { 'boosting_type': 'gbdt','class_weight': None,'colsample_bytree': 0.5,
+        params = {'boosting_type': 'gbdt', 'class_weight': None, 'colsample_bytree': 0.5,
                     'importance_type': 'split', 'learning_rate': 0.1, 'max_depth': 5,
                     'min_child_samples': 20, 'min_child_weight': 0.001, 'min_split_gain': 0.0,
                     'n_estimators': 200, 'n_jobs': -1, 'num_leaves': 10, 'reg_alpha': 0.0,
@@ -329,7 +329,6 @@ class LGBME(ModelME):
         res = self._lift(y, preds)
         return ('Lift', res, True)
     
-
 
 class LogitME(MagicE):
     def init_est(self):
@@ -350,6 +349,7 @@ class LogitME(MagicE):
         enc = OneHotEncoder(categories='auto',handle_unknown='ignore')
         enc.fit(df_cat)
         return enc
+
 
 class CartME(MagicE):
     def init_est(self):
